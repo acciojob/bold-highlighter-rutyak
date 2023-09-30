32bbc6aa0977245521889be97cb643a45565644c
rutyak <132219103+rutyak@users.noreply.github.com>
2023-09-30 11:23:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
+    const para = document.getElementById('para');
 
+    para.style.fontWeight = "bold"
 
 }
 
 
 function return_normal() {
     //Write your code here
+    const para = document.getElementById('para');
+
+    para.style.fontWeight = "normal"
 
     
 }
